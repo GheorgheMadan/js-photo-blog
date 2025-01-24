89df0ca823b43e9335a54fda199f658f1d4ced31
Gheorghe <madangheorghe.30@gmail.com>
2025-01-24 14:57:08 +0100

    create output

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // richiamo l'output dal dom 
-const output = document.querySelector('output')
+const output = document.querySelector('.output')
 
 //richiamo l'endopoint 
 const endopoint = 'https://lanciweb.github.io/demo/api/pictures/'
@@ -7,10 +7,29 @@ const endopoint = 'https://lanciweb.github.io/demo/api/pictures/'
 axios.get(endopoint)
         .then(response => {
             const card = response.data
+            // verifico in console cosa viene stampato
             console.log(card);
-            
+            // creo un ciclo for per stampare in pagina
+            for(let i = 0; i < card.length; i++){
+                let item = card[i]
+                // destrutturo 
+                const {title, date, url} = item
+
+                // pusho in pagina  
+                output.innerHTML += `
+                                    <div class="card p-3 rounded-0 shadow-lg position-relative" style="width: 20rem;">
+                                        <img src="./img/pin.svg" class="pin-dimension position-absolute shadow-lg" alt="">
+                                        <img src="${url}" alt="...">
+                                        <div class="mt-3">
+                                            <p class="text">${date}</p>
+                                            <h5 class="card-title fw-bolder">${title.toUpperCase()}</h5> 
+                                        </div>   
+                                    </div>
+                                    `
+            }
         })
         .catch(error =>{
+            // codice da eseguire in caso di errore
             console.error(error);
             
         })

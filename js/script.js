5de40c18042c857075ba4eaec0b64645d0c4ae1c
Gheorghe <madangheorghe.30@gmail.com>
2025-01-27 12:25:20 +0100

    CREATE OVERLAY

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
-// richiamo l'output dal dom 
+// richiamo l'output dal dom dove andranno stampate le card 
 const output = document.querySelector('.output')
+// richiamo l'output dal dom per l'overlay
 const outputOverlay = document.querySelector('.overlay')
 
-//richiamo l'endopoint 
-const endopoint = 'https://lanciweb.github.io/demo/api/pictures/'
+//richiamo l'endpoint 
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
 
-axios.get(endopoint)
+// attraverso axios creo la chiamata del API 
+axios.get(endpoint)
         .then(response => {
             const card = response.data
             // verifico in console cosa viene stampato
             console.log(card);
+
             // creo un ciclo for per stampare in pagina
             for(let i = 0; i < card.length; i++){
                 let item = card[i]
-                // destrutturo 
+
+                // destrutturo gli elementi
                 const {title, date, url} = item
 
                 // pusho in pagina  
@@ -27,10 +31,55 @@ axios.get(endopoint)
                                         </div>   
                                     </div>
                                     `     
-             }
+
+            }
+            // RICHIAMO TUTTE LE .card-image 
+            const images = document.querySelectorAll('.card-image');
+            
+            // CON IL CICLO FOREACH AGGIUNGO UN EVENTO AD OGNI IMMAGINE
+            images.forEach(image => {
+                    image.addEventListener('click', (event) =>{
+                        // restituiamo l'URL dell'immagine che verra cliccata dall'utente.
+                        const imgUrl = event.target.src;
+
+                         // prima pulisco l'overlay
+                        outputOverlay.innerHTML = '';
+
+
+                        // con innerHTML aggiungo in pagina l'immagine
+                        outputOverlay.innerHTML += `
+                                                    <button class="btn btn-warning overlay-button">CHIUDI</button>
+                                                    <img src="${imgUrl}" alt="..." class="card-image img-overlay">
+                                                    `;
+
+                        // RIMUOVO LA CLASSE D-NONE 
+                        outputOverlay.classList.remove('d-none');
+                        // AGGIUNGO LA CLASSE D-INLINE
+                        outputOverlay.classList.add('d-inline');
+
+                        // RICHIAMO IL PULSANTE BUTTON 
+                        const button = document.querySelector('.overlay-button')
+
+                            // creo un avento al click del button all'interno del ciclo 
+                            button.addEventListener('click', () => {
+                                // AL CLICK Nascondo l'overlay
+                                // rimuovo la classe d-inline
+                                outputOverlay.classList.remove('d-inline');
+                                // aggiungo la classe d-none
+                                outputOverlay.classList.add('d-none');
+                            });
+                    })
+            })
+                 
+            
+
         })
         .catch(error =>{
             // codice da eseguire in caso di errore
             console.error(error);
             
         })
+
+    
+
+       
